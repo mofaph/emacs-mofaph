e09ef3b769346e7b00a71d92ccf1c42d61a607ee
mofaph <mofaph@gmail.com>
2012-06-16 17:34:41 +0800

    conf-ido.el: 切换缓冲区时，不显示特定的缓冲区
    ido-mode 提供了 ido-ignore-buffers 这个变量来忽略指定的缓冲区，可以通过
    正则表达式描述不希望显示的缓冲区。
    
    如果想显示所有的缓冲区，那么在切换缓冲区时（C-x b），输入 C-a 即可显示
    所有的缓冲区。
    
    参考链接：
    
    [1] https://github.com/mjwall/emacs.d/blob/master/init-ido-mode.el
    [2] http://emacswiki.org/emacs/InteractivelyDoThings

--- conf-ido.el
+++ conf-ido.el
@@ -1,5 +1,11 @@
 (require 'ido)
 
 (ido-mode t)
+(setq ido-ignore-buffers '("\\` "
+                           "\\*scratch\\*"
+                           "Completions\\*$"
+                           "\\*Help\\*"
+                           "^\\*Ido"
+                           "^\\*Messages\\*"))
 
 (provide 'conf-ido)
