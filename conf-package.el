235f2d9bc3f69dba2153292d357484ff63a99dbb
mofaph <mofaph@gmail.com>
2012-07-04 15:49:25 +0800

    conf-package.el: 只有判断 emacs 的版本大于 24 时，才启用 package
        - 加入参考链接
        - 加入 package 源：http://melpa.milkbox.net/packages

--- conf-package.el
+++ conf-package.el
@@ -1,12 +1,18 @@
 ;; -*- coding: utf-8 -*-
 
-(require 'package)
+;; Info node: emacs(top)->Emacs Lisp Packages
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Packages.html#Packages
+;; http://ergoemacs.org/emacs/emacs_package_system.html
 
-(setq package-enable-at-startup nil)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/"))
+  ;; add at the end of list
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/") t)
 
-(package-initialize)
+  (package-initialize))
 
 (provide 'conf-package)
