7c7e3634864a59f3cf7757f6012fb4754c8d1000
mofaph <mofaph@gmail.com>
2012-07-04 15:53:58 +0800

    conf-ido.el: 打开已关闭的文件（要求 recentf）

--- conf-ido.el
+++ conf-ido.el
@@ -19,5 +19,6 @@
 (setq ido-create-new-buffer 'always)
 (setq ido-file-extensions-order '(".c" ".h" ".el" ".sh" ".cpp" ".lisp"))
 (setq ido-max-directory-size 100000)
+(setq ido-use-virtual-buffers t)
 
 (provide 'conf-ido)
