f81c2782a31faa60dc95d177008ea781e87b6c06
mofaph <mofaph@gmail.com>
2012-07-05 10:48:05 +0800

    bug fix: 修复 conf-el-get.el 中的一个异步错误
    当 el-get 没有安装的时候，打开 emacs 将会安装 el-get。这时，安装完
    el-get 后，有时 el-get 不能安装其他的插件，必须在第二次启动的时候才能安
    装完成所有的插件。
    
    出现这个问题的原因是，安装 el-get 时，使用的 url-retrieve 函数是异步执
    行的，因此就有可能出现竞争条件。
    
    修复这个错误，需要让 el-get 自身的安装和安装其他的插件能够顺序进行。也
    就是能够同步进行。
    
    同步是指：发送方发出数据后，等接收方发回响应以后才发下一个数据包的通讯方式。
    异步是指：发送方发出数据后，不等接收方发回响应，接着发送下个数据包的通讯方式。
    
    参考链接：
    
    [1] http://blog.csdn.net/OhFish/article/details/2948740

--- conf-el-get.el
+++ conf-el-get.el
@@ -1,27 +1,34 @@
 ;; -*- coding: utf-8 -*-
 
+;;; 参考链接：
+;; [1] https://github.com/DarwinAwardWinner/dotemacs/blob/master/site-lisp/el-get-init.el
+
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-;; 安装 el-get
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-     (goto-char (point-max))
-     (eval-print-last-sexp)))))
+(setq el-get-install-branch "master"
+      el-get-verbose t
+      el-get-user-package-directory "~/.emacs.d"
+      el-get-default-process-sync t)
 
-;; FIXME: 当没有安装el-get时，下面的代码将不能运行，将会出现竞争条件
+(setq packages '(
+                 goto-last-change
+                 smex
+                 switch-window
+                 ))
+
+;; 安装 el-get
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+
+;; 使用 el-get 管理其他的插件
 (when (require 'el-get nil t)
-  (setq packages '(
-                   el-get
-                   goto-last-change
-                   smex
-                   switch-window
-                   ))
   (el-get 'sync packages)
   (el-get 'wait)
-  (setq el-get-user-package-directory "~/.emacs.d")
+
   (el-get-init "goto-last-change")
   (el-get-init "smex")
   (el-get-init "switch-window")
