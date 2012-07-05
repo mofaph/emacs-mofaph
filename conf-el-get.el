5b69c77c19dcf1d297852b21dc0655244e9d1aba
mofaph <mofaph@gmail.com>
2012-07-05 15:56:31 +0800

    conf-el-get.el: 加入更多参考链接

--- conf-el-get.el
+++ conf-el-get.el
@@ -2,6 +2,7 @@
 
 ;;; 参考链接：
 ;; [1] https://github.com/DarwinAwardWinner/dotemacs/blob/master/site-lisp/el-get-init.el
+;; [2] https://github.com/dimitri/el-get/blob/master/README.asciidoc
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
