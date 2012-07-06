5b632dbb66ce71d2a22bf62c623ec704b3a99a00
mofaph <mofaph@gmail.com>
2012-07-06 10:29:23 +0800

    ibuffer: 不显示以以型号开头的缓冲区

--- conf-ibuffer.el
+++ conf-ibuffer.el
@@ -1,5 +1,10 @@
+;; http://www.emacswiki.org/emacs/IbufferMode#toc3
+
 (require 'ibuffer)
+(require 'ibuf-ext)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(add-to-list 'ibuffer-never-show-predicates "^\\*")
 
 (provide 'conf-ibuffer)
