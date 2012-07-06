ad906d1d0f525fd5ff45347a36e83121efc9ecf3
mofaph <mofaph@gmail.com>
2012-07-06 14:12:56 +0800

    defun: 使用希腊字母 lambda 替代英文 lambda

--- conf-defun.el
+++ conf-defun.el
@@ -24,4 +24,17 @@
 (add-hook 'c-mode-hook '(lambda () (define-key c-mode-map (kbd "M-j") 'open-newline-above)))
 (add-hook 'c++-mode-hook '(lambda () (define-key c++-mode-map (kbd "M-j") 'open-newline-above)))
 
+;; 使用 lambda 替代 _lambda_ （加下划线是因为在 Emacs 中设置会原地生效）
+;; http://stackoverflow.com/questions/154097/whats-in-your-emacs
+(defun sm-lambda-mode-hook ()
+  (font-lock-add-keywords
+   nil `(("\\<lambda\\>"
+          (0 (progn (compose-region (match-beginning 0) (match-end 0)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
+
+(add-hook 'emacs-lisp-mode-hook 'sm-lambda-mode-hook)
+(add-hook 'lisp-interactive-mode-hook 'sm-lamba-mode-hook)
+(add-hook 'scheme-mode-hook 'sm-lambda-mode-hook)
+
 (provide 'conf-defun)
