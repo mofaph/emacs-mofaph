3992a536a8a21c8431985bee9bdc056d7335f99d
mofaph <mofaph@gmail.com>
2012-07-08 00:20:41 +0800

    conf-package.el: 调整代码结构，使之方便阅读

--- conf-package.el
+++ conf-package.el
@@ -34,9 +34,9 @@
   (setq url-http-attempt-keepalives nil)
 
   ;; add at the end of list
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/") t)
+  (add-to-list 'package-archives '("melpa"              . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("tromey"             . "http://tromey.com/elpa/") t)
+  (add-to-list 'package-archives '("marmalade"          . "http://marmalade-repo.org/") t)
 
   (package-initialize)
 
