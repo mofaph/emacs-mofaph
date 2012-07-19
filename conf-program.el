3117edc57e0016b24ee3a245c367ef2ae0873557
mofaph <mofaph@gmail.com>
2012-07-19 16:48:59 +0800

    program: 将 newline-and-indent 绑定到 RET，使之能够自动缩进
    如果将来 electric-indent-mode 对输入 C-o 能够正常工作，那么将会使用 electric-indent-mode

--- conf-program.el
+++ conf-program.el
@@ -1,3 +1,5 @@
+;; -*- coding: utf-8 -*-
+
 ;; which-function
 (setq which-func-modes t)
 (which-function-mode t)
@@ -15,5 +17,14 @@
 ;; find-func
 (require 'find-func)
 (find-function-setup-keys)
+
+;; 自动缩进
+;; 这里没有使用 electric-indent-mode，因为这个次模式在自动缩进方面有问题
+;; 它不能很好地处理在行末或者是空白行的行首键入 C-o，它总是会缩进不该缩进的行
+(add-hook 'emacs-lisp-mode-hook (lambda () (define-key emacs-lisp-mode-map (kbd "RET") 'newline-and-indent)))
+(add-hook 'c-mode-hook (lambda () (define-key c-mode-map (kbd "RET") 'newline-and-indent)))
+(add-hook 'c++-mode-hook (lambda () (define-key c++-mode-map (kbd "RET") 'newline-and-indent)))
+(add-hook 'lisp-mode-hook (lambda () (define-key lisp-mode-map (kbd "RET") 'newline-and-indent)))
+(add-hook 'sh-mode-hook (lambda () (define-key sh-mode-map (kbd "RET") 'newline-and-indent)))
 
 (provide 'conf-program)
