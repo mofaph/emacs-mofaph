114798e7dce5ff71f1684039b41b82915e211339
mofaph <mofaph@gmail.com>
2012-07-22 10:38:05 +0800

    package.el: 修正 marmalade 的仓库地址

--- conf-package.el
+++ conf-package.el
@@ -41,7 +41,7 @@
   ;; add at the end of list
   (add-to-list 'package-archives '("melpa"              . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives '("tromey"             . "http://tromey.com/elpa/") t)
-  (add-to-list 'package-archives '("marmalade"          . "http://marmalade-repo.org/") t)
+  (add-to-list 'package-archives '("marmalade"          . "http://marmalade-repo.org/packages/") t)
 
   (package-initialize)
 
