f8e2154b8ab05cf19440ff7a75a508d261805677
mofaph <mofaph@gmail.com>
2012-07-26 14:45:41 +0800

    defun: 增加一个复制正则匹配文本的函数

--- conf-defun.el
+++ conf-defun.el
@@ -38,4 +38,22 @@
 (add-hook 'lisp-interactive-mode-hook 'sm-lamba-mode-hook)
 (add-hook 'scheme-mode-hook 'sm-lambda-mode-hook)
 
+;; 拷贝正则匹配的文本到 kill-ring，同时匹配的文本显示在一个缓冲区中
+;; http://stackoverflow.com/questions/2289883/emacs-copy-matching-lines
+(defun copy-lines-matching-re (re)
+  "find all lines matching the regexp RE in the current buffer
+putting the matching lines in a buffer named *matching*"
+  (interactive "sRegexp to match: ")
+  (let ((result-buffer (get-buffer-create "*matching*")))
+    (with-current-buffer result-buffer
+      (erase-buffer))
+    (save-match-data
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward re nil t)
+          (princ (buffer-substring-no-properties (line-beginning-position)
+                                                 (line-beginning-position 2))
+                 result-buffer))))
+    (pop-to-buffer result-buffer)))
+
 (provide 'conf-defun)
