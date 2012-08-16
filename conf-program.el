b4752a0786004e9e1b32443031ff234f5b668ae5
mofaph <mofaph@gmail.com>
2012-08-16 10:59:26 +0800

    program: 将需要配置的主模式和主模式的钩子（hook）分别放到变量中
    其中，钩子 lang-mode-hook 是根据 lang-mode 生成的列表。这样，如果需要修
    改 lang-mode，只需要编辑一次就可以了。

--- conf-program.el
+++ conf-program.el
@@ -18,25 +18,31 @@
 (require 'find-func)
 (find-function-setup-keys)
 
+;;; 定义一些语言相关的变量
+
+(defvar lang-mode '(c-mode
+                    c++-mode
+                    emacs-lisp-mode
+                    lisp-mode
+                    java-mode
+                    objc-mode
+                    sh-mode)
+  "The programming language I usually edit.")
+
+(defvar lang-mode-hook
+  (mapcar (lambda (mode)
+            (intern (concat (symbol-name mode) "-hook")))
+          lang-mode)
+  "The programming language hook I usually edit.")
+
 ;; 自动缩进
 ;; 这里没有使用 electric-indent-mode，因为这个次模式在自动缩进方面有问题
 ;; 它不能很好地处理在行末或者是空白行的行首键入 C-o，它总是会缩进不该缩进的行
-(dolist (hook '(emacs-lisp-mode-hook
-                c-mode-hook
-                c++-mode-hook
-                lisp-mode-hook
-                objc-mode-hook
-                java-mode
-                sh-mode-hook))
+(dolist (hook lang-mode-hook)
   (add-hook hook (lambda () (local-set-key (kbd "RET") 'newline-and-indent))))
 
 ;; 高亮 FIXME 关键字
-(dolist (mode '(c-mode
-                c++-mode
-                emacs-lisp-mode
-                lisp-mode
-                java-mode
-                sh-mode))
+(dolist (mode lang-mode)
   (font-lock-add-keywords mode
                           '(("\\<\\(FIXME\\):?\\>" 1 font-lock-warning-face t))))
 
