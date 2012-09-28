3b2f8a86b5698eb4dd6866805a2c457d8d3fbefe
mofaph <mofaph@gmail.com>
2012-09-28 13:53:01 +0800

    auto-complete: 添加 c c++ perl python shell 的支持

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -16,6 +16,46 @@
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
 
+;; default sources:
+;;     ac-source-abbrev
+;;     ac-source-dictionary
+;;     ac-source-words-in-same-mode-buffers
+;;
+;; builtin setup:
+;;     emacs-lisp-mode
+;;     cc-mode
+;;     ruby-mode
+;;     css-mode
+;;
+;; see auto-complete-conf.el for more detail.
+
+;; redefine ac-cc-mode-setup
+;; original define is in auto-complete-conf.el
+(defun ac-cc-mode-setup ()
+  (setq ac-sources (append ac-sources '(ac-source-imenu ; add
+                                        ac-source-yasnippet
+                                        ac-source-gtags
+                                        ac-source-semantic ; add
+                                        ))))
+
+;; shell
+(defun ac-sh-mode-setup ()
+  (setq ac-sources (append ac-sources '(ac-source-imenu
+                                        ac-source-yasnippet))))
+(add-hook 'sh-mode-hook 'ac-sh-mode-setup)
+
+;; perl
+(defun ac-perl-mode-setup ()
+  (setq ac-sources (append ac-sources '(ac-source-imenu
+                                        ac-source-yasnippet))))
+(add-hook 'perl-mode-hook 'ac-perl-mode-setup)
+
+;; python
+(defun ac-python-mode-setup ()
+  (setq ac-sources (append ac-sources '(ac-source-imenu
+                                        ac-source-yasnippet))))
+(add-hook 'python-mode-hook 'ac-python-mode-setup)
+
 (ac-clear-dictionary-cache)
 
 (provide 'init-auto-complete)
