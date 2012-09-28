a1db4624b7ee4bed3a88d1ca83707bbc53f34eeb
mofaph <mofaph@gmail.com>
2012-09-28 13:45:38 +0800

    auto-complete: 使用默认的配置

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -2,19 +2,10 @@
 
 (require 'auto-complete)
 (require 'auto-complete-config)
-(ac-config-default)
-(global-auto-complete-mode t)
+(ac-config-default) ; also enable global-auto-complete-mode
 
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/.ac-dict")
 (setq ac-user-dictionary-files "~/.emacs.d/.dict")
-
-(set-default 'ac-sources
-             '(ac-source-abbrev
-               ac-source-dictionary
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-words-in-all-buffer
-               ac-source-imenu))
 
 (setq ac-auto-show-menu nil)
 (setq ac-auto-start nil)
@@ -24,15 +15,6 @@
 (define-key ac-mode-map (kbd "M-/") 'auto-complete)
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
-
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (dolist (mode '(ac-source-features
-                            ac-source-functions
-                            ac-source-symbols
-                            ac-source-gtags
-                            ac-source-semantic)
-                          (add-to-list 'ac-sources mode)))))
 
 (ac-clear-dictionary-cache)
 
