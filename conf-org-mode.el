ccd15809d2c42f84f18e71441034395abf19b0f5
mofaph <mofaph@gmail.com>
2012-10-24 11:43:01 +0800

    org: TODO 和时间的设置
        - TODO: 当子树中还有未完成的 TODO 时，上一层的 TODO 不能标记为 DONE
        - Date/Times: 当开始计时，Emacs 空闲 10 分钟，就给出提示
        - Date/Times: 当开始计时，期间关闭 Emacs，则在退出前保留计时状态

--- conf-org-mode.el
+++ conf-org-mode.el
@@ -15,4 +15,21 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c l") 'org-store-link)
 
+
+
+;;; TODO
+
+(setq org-enforce-todo-dependencies t)  ; 当子任务还有 TODO 标志时，不能将任务设置为 DONE
+
+
+
+;;; Date/Times
+
+;; 当空闲时间超过 10 分钟，就给出提示
+(setq org-clock-idle-time 10)
+
+;; 在关闭 Emacs 后，重新计时
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+
 (provide 'conf-org-mode)
