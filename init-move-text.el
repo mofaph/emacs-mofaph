378f8578a7c7a610c983fb0b556ade35d68c5586
mofaph <mofaph@gmail.com>
2013-04-07 20:58:53 +0800

    move-text: 某些模式占用了 M-up 和 M-down 这两个快捷键，覆盖这些设置

--- init-move-text.el
+++ init-move-text.el
@@ -1,6 +1,20 @@
+;; -*- coding: utf-8; -*-
+
 (require 'move-text)
 
 (global-set-key [M-up] 'move-text-up)
 (global-set-key [M-down] 'move-text-down)
+
+;;; 有些模式占用了 M-up 和 M-down，在这里覆盖这些配置
+
+(defun move-text-setting-key-binding ()
+  "Key binding that move-text should set."
+  (local-set-key [M-up]         'move-text-up)
+  (local-set-key [M-down]       'move-text-down))
+
+(dolist  (hook '(
+                 org-mode-hook
+                 ))
+  (add-hook hook 'move-text-setting-key-binding))
 
 (provide 'init-move-text)
