ebb56b2f0d217f3401bbe33df79c2f10bfc9c386
mofaph <mofaph@gmail.com>
2013-06-10 10:20:02 +0800

    paredit: 允许使用 move-text 的 M-up 和 M-down 移动文本

--- init-paredit.el
+++ init-paredit.el
@@ -9,6 +9,10 @@
      (define-key paredit-mode-map (kbd "C-j") 'open-newline-indent)
      (define-key paredit-mode-map (kbd "M-j") 'open-newline-above)
 
+     (when (require 'move-text nil t)
+       (define-key paredit-mode-map (kbd "<M-up>") 'move-text-up)
+       (define-key paredit-mode-map (kbd "<M-down>") 'move-text-down))
+
      ;; lisp-related-mode-hook: global-variable.el
      ;; FIXME: 不能在 ielm-mode 中启用 paredit
      (dolist (hook lisp-related-mode-hook)
