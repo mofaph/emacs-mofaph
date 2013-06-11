63dcb59abe17d6d0b97c53500cdbd6fb0aa028f0
mofaph <mofaph@gmail.com>
2013-06-11 13:26:17 +0800

    keyfreq: 按键的频率应该同步
    按照以下的顺序读取 keyfreq-file：
    
            o KEYFREQ_FILE （环境变量）
            o ~/Dropbox/conf/.keyfreq
            o ~/.emacs.d/.keyfreq
            o 默认值
    
    由于按键的频率是在全局的情况下才有意义，因此应该同步。而同步的文件，应该可以指定，
    所以从环境变量中得到。

--- init-keyfreq.el
+++ init-keyfreq.el
@@ -1,6 +1,19 @@
 (require 'keyfreq)
 
-(setq keyfreq-file "~/.emacs.d/.keyfreq")
+(let ((file1 (getenv "KEYFREQ_FILE"))
+      (file2 "~/Dropbox/conf/.keyfreq")
+      (file3 "~/.emacs.d/.keyfreq"))
+  (cond
+   (file1
+    (setq keyfreq-file file1))
+   ((file-exists-p file2)
+    (setq keyfreq-file file2))
+   ((file-exists-p file3)
+    (setq keyfreq-file file3))
+   (t
+    (message "init-keyfreq: keyfreq-file leave to default."))))
+
+(setq keyfreq-file-lock "~/.emacs.d/.keyfreq.lock")
 
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
