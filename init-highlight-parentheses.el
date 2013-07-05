c1576d8a43a1bd4745158e629f952d2e35f9302b
mofaph <mofaph@gmail.com>
2013-07-05 09:55:42 +0800

    highlight-parentheses: 高亮的括号加入背景色，这是 Lisper 之福！

--- init-highlight-parentheses.el
+++ init-highlight-parentheses.el
@@ -1,5 +1,16 @@
 (require 'highlight-parentheses)
 
-(highlight-parentheses-mode)
+; (setq hl-paren-colors '("blue" "red" "thistle" "orange"))
+
+(setq hl-paren-colors '("black" "black" "black" "black"))
+
+(setq hl-paren-background-colors '(
+                                   "turquoise"
+                                   "salmon"
+                                   "orchid"
+                                   "light pink"
+                                   ))
+
+(add-hook 'prog-mode-hook 'highlight-parentheses-mode)
 
 (provide 'init-highlight-parentheses)
