2c5da63a1d4a3e5f44b64756dc77400f06b0ea39
mofaph <mofaph@gmail.com>
2013-07-07 00:47:06 +0800

    magit: 配置 Git 仓库的目录搜索列表
    这样 C-u magit-status 就可以给出列表可供选择了

--- init-magit.el
+++ init-magit.el
@@ -3,6 +3,14 @@
 
 (require 'magit)
 
+(setq magit-repo-dirs '(
+                        "/work"
+                        "~/github"
+                        "~/bitbucket"
+                        "~/scm/git"
+                        "~/bigstore/scm/git"
+                        ))
+
 (global-set-key (kbd "C-x C-z") 'magit-status)
 (global-set-key (kbd "C-c m s") 'magit-status)
 (global-set-key (kbd "C-c m g") 'magit-grep)
