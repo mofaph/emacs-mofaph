731417a517755e12c1a90a4bf08846d1467f0203
mofaph <mofaph@gmail.com>
2014-04-30 21:23:59 +0800

    mc/mark-more-like-this-extended ==> C-S-m

--- init-multiple-cursors.el
+++ init-multiple-cursors.el
@@ -5,5 +5,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-*") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-m") 'mc/mark-more-like-this-extended)
 
 (provide 'init-multiple-cursors)
