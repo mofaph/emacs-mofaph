0890e808402bc37ae751826071915fe49de52ab6
mofaph <mofaph@gmail.com>
2014-04-30 21:33:58 +0800

    helm: add personal key binding

--- init-helm.el
+++ init-helm.el
@@ -1,5 +1,15 @@
 (require 'helm)
 
-(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "M-1") 'helm-etags-select)
+(global-set-key (kbd "M-2") 'helm-buffers-list)
+(global-set-key (kbd "M-3") 'helm-find-files)
+(global-set-key (kbd "C-c h a") 'helm-apropos)
+(global-set-key (kbd "C-c h f") 'helm-for-files)
+(global-set-key (kbd "C-c h g") 'helm-do-grep)
+(global-set-key (kbd "C-c h i") 'helm-imenu)
+(global-set-key (kbd "C-c h C-@") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h m") 'helm-man-woman)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h r") 'helm-register)
 
 (provide 'init-helm)
