d6f29437105e8bdc162f1b2c29c5ec747328890a
mofaph <mofaph@gmail.com>
2014-05-17 21:06:32 +0800

    xcscope: M-k ==> kill-buffer-no-comfirm

--- init-xcscope.el
+++ init-xcscope.el
@@ -1,3 +1,9 @@
 (require 'xcscope)
+
 (cscope-setup)
+
+(eval-after-load 'xcscope
+  '(progn
+     (define-key cscope-list-entry-keymap (kbd "M-k") 'kill-buffer-no-comfirm)))
+
 (provide 'init-xcscope)
