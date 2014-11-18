697ceba1d13104e4f5abc45bdd54e3e755aefc8f
mofaph <mofaph@gmail.com>
2014-11-18 15:30:39 +0800

    session: fixed a frame restore bug
    假设屏幕的大小是这样：
    
            ----------------
            |              |
            |              |
            |     Emacs    |
            |              |
            |              |
            |              |
            ----------------
    
    窗口在下次启动时，会变成这样：
    
            ----------------
            |       |      |
            | Emacs |      |
            |       |      |
            |--------      |
            |              |
            |              |
            ----------------

--- personal/conf-session.el
+++ personal/conf-session.el
@@ -3,6 +3,7 @@
 (setq-default desktop-dirname "~/.emacs.d")
 (setq-default desktop-load-locked-desktop t)
 (setq desktop-save 'if-exists)
+(setq desktop-restore-reuses-frames nil)
 (desktop-save-mode 1)
 (desktop-read "~/.emacs.d")
 
