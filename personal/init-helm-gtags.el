2eaafa9ee501cb88cec121c29bdd031a0ecdc34e
mofaph <mofaph@gmail.com>
2015-01-01 18:54:39 +0800

    helm-gtags: no auto update

--- personal/init-helm-gtags.el
+++ personal/init-helm-gtags.el
@@ -2,7 +2,7 @@
 (require 'helm-gtags)
 
 (setq helm-gtags-ignore-case t
-      helm-gtags-auto-update t
+      ;; helm-gtags-auto-update t
       helm-gtags-use-input-at-cursor t
       helm-gtags-pulse-at-cursor t
       helm-gtags-suggested-key-mapping t)
