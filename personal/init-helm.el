7a0478ffdc688cb1c7d6a510ebd52e51bc687b36
mofaph <mofaph@gmail.com>
2015-01-11 17:32:46 +0800

    helm: change some personal bindings
    helm-mini ==> M-2
    helm-all-mark-rings ==> C-c h SPC

--- personal/init-helm.el
+++ personal/init-helm.el
@@ -13,7 +13,7 @@
       helm-ff-file-name-history-use-recentf t)
 
 (global-set-key (kbd "M-1") 'helm-imenu)
-(global-set-key (kbd "M-2") 'helm-buffers-list)
+(global-set-key (kbd "M-2") 'helm-mini)
 (global-set-key (kbd "M-3") 'helm-find-files)
 ;;; M-4 helm-projectile
 (global-set-key (kbd "M-5") 'helm-bookmarks)
@@ -22,7 +22,7 @@
 (global-set-key (kbd "C-c h f") 'helm-for-files)
 (global-set-key (kbd "C-c h g") 'helm-do-grep)
 (global-set-key (kbd "C-c h i") 'helm-imenu)
-(global-set-key (kbd "C-c h C-@") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-c h m") 'helm-man-woman)
 (global-set-key (kbd "C-c h o") 'helm-occur)
 (global-set-key (kbd "C-c h r") 'helm-register)
