111720e75f9043955843dd5d9694e95e0cc85e6f
mofaph <mofaph@gmail.com>
2015-04-01 09:15:19 +0800

    projectile: use helm as complete backend

--- personal/init-projectile.el
+++ personal/init-projectile.el
@@ -1,6 +1,10 @@
 (require 'projectile)
+(require 'helm-projectile)
 
 (projectile-global-mode)
+
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
 
 (global-set-key (kbd "M-4") 'helm-projectile)
 
