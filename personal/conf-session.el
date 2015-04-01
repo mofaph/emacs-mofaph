9a4ab91f239683f53deeebd374ac240156089fe6
mofaph <mofaph@gmail.com>
2015-04-01 09:49:28 +0800

    session: use user-emacs-directory instead of ~/.emacs.d

--- personal/conf-session.el
+++ personal/conf-session.el
@@ -1,10 +1,10 @@
 ;; desktop
 (load "desktop")
-(setq-default desktop-dirname "~/.emacs.d")
+(setq-default desktop-dirname user-emacs-directory)
 (setq-default desktop-load-locked-desktop t)
 (setq desktop-save 'if-exists)
 (setq desktop-restore-reuses-frames nil)
 (desktop-save-mode 1)
-(desktop-read "~/.emacs.d")
+(desktop-read user-emacs-directory)
 
 (provide 'conf-session)
