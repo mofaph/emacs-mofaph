4d1afff848cad200d473977d9f363e5ee5320e0d
mofaph <mofaph@gmail.com>
2016-12-25 19:07:55 +0800

    move-text: 增加 "ESC <up|down>" 以便于终端可以使用

--- personal/init-move-text.el
+++ personal/init-move-text.el
@@ -2,15 +2,20 @@
 
 (require 'move-text)
 
+(global-set-key (kbd "ESC <up>") 'move-text-up)
 (global-set-key [M-up] 'move-text-up)
+
+(global-set-key (kbd "ESC <down>") 'move-text-down)
 (global-set-key [M-down] 'move-text-down)
 
 ;;; 有些模式占用了 M-up 和 M-down，在这里覆盖这些配置
 
 (defun move-text-setting-key-binding ()
   "Key binding that move-text should set."
-  (local-set-key [M-up]         'move-text-up)
-  (local-set-key [M-down]       'move-text-down))
+  (local-set-key [M-up]                 'move-text-up)
+  (local-set-key (kbd "ESC <up>")       'move-text-up)
+  (local-set-key [M-down]               'move-text-down)
+  (local-set-key (kbd "ESC <down>")     'move-text-down))
 
 (dolist  (hook '(
                  org-mode-hook
