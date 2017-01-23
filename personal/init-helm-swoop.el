24d9e1e8eefa8cbc48ad463311c7d441346ecfe4
mofaph <mofaph@gmail.com>
2017-01-23 13:43:38 +0800

    helm-swoop: 搜索时新开一个窗口，而不是占用已经打开的窗口

--- personal/init-helm-swoop.el
+++ personal/init-helm-swoop.el
@@ -4,4 +4,7 @@
 (global-set-key (kbd "C-c h s") 'helm-swoop)
 (global-set-key (kbd "C-c h a") 'helm-multi-swoop-all)
 
+;; If this value is t, split window inside the current window
+(setq helm-swoop-split-with-multiple-windows t)
+
 (provide 'init-helm-swoop)
