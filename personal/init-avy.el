402326bc0271065868aead132803d1373dac7ea1
mofaph <mofaph@gmail.com>
2018-06-05 10:55:09 +0800

    avy: rebind-goto-char-2, avy-goto-line, remove avy-goto-word-0
    avy-goto-char-2 require enter 2 char, but it show less candidates. Bind
    it to more convinence key binding: M-g M-g.
    
    bind avy-goto-line to M-g M-a.
    
    remove avy-goto-word-0, I don't use it because it shows too many
    candidates.

--- personal/init-avy.el
+++ personal/init-avy.el
@@ -1,8 +1,7 @@
 (require 'avy)
 
-(global-set-key (kbd "M-g M-g") 'avy-goto-char)
-(global-set-key (kbd "M-g M-w") 'avy-goto-word-0)
-(global-set-key (kbd "M-g l") 'avy-goto-line)
+(global-set-key (kbd "M-g M-g") 'avy-goto-char-2)
+(global-set-key (kbd "M-g M-a") 'avy-goto-line)
 
 (global-set-key (kbd "M-g M-q") 'avy-pop-mark)
 
