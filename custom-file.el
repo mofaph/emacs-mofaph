73e0afa5a0bd68a90e07661521200323ea3366f9
mofaph <mofaph@gmail.com>
2018-07-11 18:05:05 +0800

    add custom-file.el, configure magit and helm face

---
+++ custom-file.el
@@ -0,0 +1,11 @@
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "#073642"))))
+ '(helm-selection-line ((t (:inherit highlight :distant-foreground "black"))))
+ '(magit-item-highlight ((t (:inherit nil))))
+ '(magit-key-mode-args-face ((t nil)))
+ '(magit-tag ((t nil))))
