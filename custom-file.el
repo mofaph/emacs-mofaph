a066e0bf08eaa2a7091cfabde184fbce50d36f00
mofaph <mofaph@gmail.com>
2018-07-11 18:07:41 +0800

    custom-file.el: add theme sanityinc-solarized-dark

--- custom-file.el
+++ custom-file.el
@@ -9,3 +9,11 @@
  '(magit-item-highlight ((t (:inherit nil))))
  '(magit-key-mode-args-face ((t nil)))
  '(magit-tag ((t nil))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
