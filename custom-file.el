0b44b5417a6d59a8d56e07d5ec70a7c234876333
mofaph <mofaph@gmail.com>
2018-07-12 12:30:51 +0800

    magit: remove unwork face config

--- custom-file.el
+++ custom-file.el
@@ -4,8 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-item-highlight ((t (:inherit nil))))
- '(magit-item-mark ((t (:inherit nil))))
  '(magit-diff-added ((t (:foreground "green"))))
  '(magit-diff-removed ((t (:foreground "red")))))
 
