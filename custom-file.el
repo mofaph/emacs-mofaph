1ad71a0679ee7de3b7d074cd5f2a2d58442344a1
mofaph <mofaph@gmail.com>
2018-07-12 14:15:01 +0800

    init.el: set a comfortable cursor face

--- custom-file.el
+++ custom-file.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "light blue"))))
  '(magit-diff-added ((t (:foreground "green"))))
  '(magit-diff-removed ((t (:foreground "red")))))
 
