32c3b620ca0300e77af82b72486aaa6435f650db
mofaph <mofaph@gmail.com>
2018-07-16 09:29:21 +0800

    init.el: 定义的变量移到使用它的地方附近

--- init.el
+++ init.el
@@ -226,30 +226,6 @@
 ;;          (cmake-ide-build-dir . "/path/to/project/build/root"))))
 
 ;; ----------------------------------------
-;; 全局变量，自定义加载 el 文件的目录
-;; ----------------------------------------
-
-(defvar c-related-mode '(c++-mode c-mode java-mode objc-mode)
-  "The C related mode.")
-
-(defvar c-related-mode-hook
-  (mapcar (lambda (mode)
-            (intern (concat (symbol-name mode) "-hook")))
-          c-related-mode)
-  "The C related mode hook.")
-
-(defvar lisp-related-mode '(clojure-mode common-lisp-mode emacs-lisp-mode eshell-mode
-                            inferior-emacs-lisp-mode lisp-interaction-mode lisp-mode
-                            scheme-mode slime-repl-mode)
-  "The Lisp related mode.")
-
-(defvar lisp-related-mode-hook
-  (mapcar (lambda (mode)
-            (intern (concat (symbol-name mode) "-hook")))
-          lisp-related-mode)
-  "The Lisp related mode hook.")
-
-;; ----------------------------------------
 ;; 基本设置
 ;; ----------------------------------------
 
@@ -444,6 +420,30 @@
 (bind-key* "M-0" 'compile)
 (bind-key* "<f6>" 'backward-paragraph)
 (bind-key* "<f8>" 'forward-paragraph)
+
+;; ----------------------------------------
+;; 自定义函数绑定快捷键需要覆盖的模式
+;; ----------------------------------------
+
+(defvar c-related-mode '(c++-mode c-mode java-mode objc-mode)
+  "The C related mode.")
+
+(defvar c-related-mode-hook
+  (mapcar (lambda (mode)
+            (intern (concat (symbol-name mode) "-hook")))
+          c-related-mode)
+  "The C related mode hook.")
+
+(defvar lisp-related-mode '(clojure-mode common-lisp-mode emacs-lisp-mode eshell-mode
+                            inferior-emacs-lisp-mode lisp-interaction-mode lisp-mode
+                            scheme-mode slime-repl-mode)
+  "The Lisp related mode.")
+
+(defvar lisp-related-mode-hook
+  (mapcar (lambda (mode)
+            (intern (concat (symbol-name mode) "-hook")))
+          lisp-related-mode)
+  "The Lisp related mode hook.")
 
 ;; ----------------------------------------
 ;; 以下是自定义的小函数的快捷键绑定
